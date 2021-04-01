64cf3c922bd1e1310530f77cb4f6aebfa37590ad
lowieCOOL <lowie.vang@gmail.com>
2021-04-01 12:11:12 +0200

    Walls now change to red when colliding with player.

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -7,5 +7,6 @@ public class ObjectHit : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
         Debug.Log("You bumped into the wall");
+        GetComponent<MeshRenderer>().material.color = Color.red;
     }
 }
